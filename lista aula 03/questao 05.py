43c2d92ebbee85aa6f16ad5f45c858700127755f
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-05 16:37:42 -0300

    questao 05

---
+++ lista aula 03/questao 05.py
@@ -0,0 +1,12 @@
+from  time import sleep 
+import os
+x = 10
+while x >= 0:
+    for contagem in range(2):
+        sleep(1)
+        print(x)
+        x = x-1
+        
+os.system('clear')  
+print('Fogo')
+    
