e3dace3ba72e4deb93a7348841a09067a4cdf425
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-06 14:31:40 -0300

    questao 02

--- lista aula 03/questao 02.py
+++ lista aula 03/questao 02.py
@@ -2,11 +2,11 @@ def aumento_salarial(salario):
 
     if salario > 1250:
         aumento = salario + (0.10 * salario)
-        print("Seu salario com reajuste de 10% vai para {:.2f}".format(aumento))
+        print(f"Seu salario com reajuste de 10% vai para {aumento}")
     
     else:
         aumento = salario + (0.15 *salario)
-        print("Seu salario com reajuste de 15% vai para {:.2f}".format(aumento))
+        print(f"Seu salario com reajuste de 15% vai para {aumento}")
 
 salario= float(input("Diga qual seu salario atual: R$ "))
 aumento_salarial(salario)
