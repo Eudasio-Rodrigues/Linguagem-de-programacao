78b784c8d0c02f866b3b6760daa35f20a6de1eaa
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-07 15:53:17 -0300

    questao 05

--- lista aula 03/questao 05.py
+++ lista aula 03/questao 05.py
@@ -1,7 +1,10 @@
+#Faça um programa para escrever a contagem regressiva do lançamento de um foguete. 
+#O programa deve imprimir 10, 9, 8..., 1, 0 e "Fogo!", na tela.
+
 from  time import sleep 
 import os
 x = 10
-while x > -1:
+while x > 0:
     for contagem in range(2):
         sleep(1)
         print(x)
