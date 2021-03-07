53c8d6182c4ad8f6c88522709b8ca99121e72aa8
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-07 15:49:57 -0300

    questao 03

--- lista aula 03/questao 03.py
+++ lista aula 03/questao 03.py
@@ -1,3 +1,7 @@
+# Escreva um programa que pergunte a distância que um passageiro deseja
+# percorrer em km. Calcule o preço da passagem, cobrando R$ 0.50 por km
+# para viagens de até 200 km, e R$ 0.45 para viagens mais longas.
+
 distancia= int(input("Digite a distancia da sua viagem. Km: "))
 if distancia <= 200:
     passagem = distancia * 0.50
