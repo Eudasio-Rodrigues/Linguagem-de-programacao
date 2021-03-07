376809501801305ac0e262b57b177c591d9afe0b
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-07 15:55:31 -0300

    questao 06

--- lista aula 03/questao 06.py
+++ lista aula 03/questao 06.py
@@ -1,5 +1,10 @@
+#Utilizando while imprima os resultados da tabuada de 2:
+#A saída deve ser semelhante a:
+#2 x 1 = 2
+#2 x 2 = 4
+
 tabuada = 2
 auxiliar = 0
 while auxiliar <= 10:
     print(f"{tabuada} X {auxiliar} = {tabuada * auxiliar}")
-    auxiliar= auxiliar + 1 
+    auxiliar = auxiliar + 1 
