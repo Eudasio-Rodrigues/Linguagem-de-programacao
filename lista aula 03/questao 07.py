ca617c210db29ce9275681269bc78dda5fa41a63
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-07 15:56:56 -0300

    questao 07

--- lista aula 03/questao 07.py
+++ lista aula 03/questao 07.py
@@ -1,3 +1,8 @@
+#Escreva um programa que leia números inteiros do teclado. 
+#O programa deve ler os números até que o usuário digite 0. 
+#No final da execução, exiba a quantidade de números digitados, 
+#assim como a soma e a média aritmética
+
 loop = True
 lista = []
 
