f12373d4dc01afc14c6e626477f5f6deaf948e34
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-07 15:20:13 -0300

    questao 09

---
+++ lista aula 03/questao 09.py
@@ -0,0 +1,38 @@
+lista_impares=[]
+lista_pares=[]
+lista_multiplos = []
+lista_primos= []
+
+#lista de numeros impares
+for i in range(10,120):
+    if i % 2 != 0:
+        lista_impares.append(i)
+print(f"LISTA DOS NUMEROS ÍMPARES: {lista_impares}\n\n")
+
+#lista de numeros pares
+for i in range(2,1001):
+    if i % 2 == 0:
+        lista_pares.append(i)
+print(f"LISTA DOS NUMEROS PARES: {lista_pares}\n\n")
+
+#lista de numeros multiplos de 2
+for i in range(40,101):
+    if i % 2 == 0:
+        
+        lista_multiplos.append(i)
+lista_multiplos.reverse()
+print(f"LISTA DOS MULTIPLOS DE 2 DECRESCENTE: {lista_multiplos}\n\n")
+
+#lista de primos de 44 ate 99
+lista_primos = []
+
+for x in range(44,100):
+    cont=0
+
+    for y in range(1, x+1):
+        if x % y == 0:
+            cont += 1
+    if cont <= 2:
+            lista_primos.append(x)
+
+print(F"LISTA DOS PRIMOS DE 44 ATÉ 99: {lista_primos}")
