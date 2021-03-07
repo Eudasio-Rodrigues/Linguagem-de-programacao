410d5f1521c8068be15cf5d78adccefb6e2943d0
Eudasio Rodrigues <79660796+Eudasio-Rodrigues@users.noreply.github.com>
2021-03-07 15:32:26 -0300

    Update questao 01.py

--- lista aula 03/questao 01.py
+++ lista aula 03/questao 01.py
@@ -1,8 +1,3 @@
-#Escreva uma função que simule o funcionamento de um radar eletrônico. 
-#Essa função deve receber a velocidade do carro de um usuário. 
-#Caso ultrapasse 80 Km/h, exiba uma mensagem dizendo que o usuário foi multado. 
-#Nesse caso, exiba o valor da multa, cobrando R$ 90 reais pela infração + R$ 5 reais por km acima de 80 km/h
-
 def radar_eletronico(velocidade):
     km_rodado=5 #valor da ,ulta por km rodado
 
