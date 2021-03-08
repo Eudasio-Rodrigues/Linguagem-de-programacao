33edfadfcb5016ec1bcf5212a7534a167ef442c0
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-08 14:55:28 -0300

    conversao de expressoes matematicas

---
+++ lista aula 02/Questao 01.py
@@ -0,0 +1,28 @@
+#Converta as seguintes expressões matemáticas para que possam ser calculadas usando Python:
+#10 + 20 x 30
+#4² / 3
+#(9⁴ + 2) x 6 -1
+#(9⁴ + 2)⁴ + ( 10 / 1)
+
+x=10
+y=20
+z=30
+
+print(f"10 + 20 * 30 = {x + y * z}")
+
+a = 4
+b = 3
+
+print(f"4² / 3 = {a**2 / b:.2f}")
+
+c = 9
+d = 2
+e = 6
+f = 1
+print(f"(9⁴ + 2) x 6 -1 = {(9**4 + 2) * 6 - 1}")
+
+m = 9
+n = 2
+o = 10 
+p = 1
+print(f"(9⁴ + 2)⁴ + ( 10 / 1) = {(9**4 + 2) + (10 / 1)}")
