26fa056ac2545a879b9cb71829dc7ab7c44d7fe3
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-10 20:04:05 -0300

    pratica

--- pratica.py
+++ pratica.py
@@ -1,16 +1,22 @@
-n1= int(input("lado 1: "))
+'''n1= int(input("lado 1: "))
 n2= int(input("lado 2: "))
 n3= int(input("lado 3: "))
 
-'''def perimetro(n1,n2,n3=10):
+def perimetro(n1,n2,n3=10):
     return n1+n2+n3
-'''
+
 perimetro = lambda n1,n2,n3: n1+n2+n3
 
 print(perimetro(n1,n2,n3))
 
-'''def imprimir_perimetro(perimetro):
+def imprimir_perimetro(perimetro):
     print(perimetro)
 
 imprimir_perimetro(perimetro(n1,n2))
 '''
+Nome="eudasio"
+nome="edinara"
+
+print(Nome,nome)
+
+12 += 3  12 **= 3 12 -+ 3 12 *= (12+3)
