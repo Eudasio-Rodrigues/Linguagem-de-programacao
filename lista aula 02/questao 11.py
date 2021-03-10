1fa4865c98a55d4fae3df4911c25a94782123e20
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-10 20:05:02 -0300

    questao 11

---
+++ lista aula 02/questao 11.py
@@ -0,0 +1,7 @@
+#Escreva um programa que receba as medidas dos lados de um triângulo e calcule sua área e perímetro.
+
+lado1= int(input("Lado 1 do triangulo\n"))
+lado2= int(input("Lado 2 do triangulo\n"))
+lado2= int(input("Lado 3 do triangulo\n"))
+
+area
