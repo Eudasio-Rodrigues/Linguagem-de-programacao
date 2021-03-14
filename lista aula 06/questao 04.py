acbfce090ccda7da47723bc0f722158eac05f039
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-14 15:43:30 -0300

    tuplas

---
+++ lista aula 06/questao 04.py
@@ -0,0 +1,10 @@
+#Faça uma função que receba uma lista e 3 números, esses números devem ser 
+#índices válidos dessa lista. A função deve retornar uma tupla composta 
+#pelos valores dos índices indicados.
+
+def converte(lista): 
+    return tuple(list) 
+   
+list = [1, 2, 3] 
+converte(list)
+print(converte(list)) 
