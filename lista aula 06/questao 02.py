faeb66d4d6dd17d24fca52e93b8ddea4118ea9ac
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-14 14:17:24 -0300

    retirando numeros repetidos

---
+++ lista aula 06/questao 02.py
@@ -0,0 +1,15 @@
+#Faça o mesmo que que se pede na questão anterior
+#mas a terceira lista não pode ter itens repetidos.
+
+lista1 = [int(input("Lista 1: ")) for i in range(5)]
+lista2 = [int(input("Lista 2: ")) for i in range(5)]
+
+
+def lista_final(lista1,lista2):
+    lista3 = []
+    lista3.extend(lista1)
+    lista3.extend(lista2)
+    lista3 = sorted(set(lista3))
+    print(f"{lista3}")
+
+lista_final(lista1,lista2)
