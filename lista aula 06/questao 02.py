2170fe2152c253fade83af97ed32e4813bdff6ba
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-16 15:18:44 -0300

    listas sem repetiçao

--- lista aula 06/questao 02.py
+++ lista aula 06/questao 02.py
@@ -9,7 +9,7 @@ def lista_final(lista1,lista2):
     lista3 = []
     lista3.extend(lista1)
     lista3.extend(lista2)
-    lista3 = sorted(set(lista3))
+    lista3 = set(lista3)
     print(f"{lista3}")
 
 lista_final(lista1,lista2)
