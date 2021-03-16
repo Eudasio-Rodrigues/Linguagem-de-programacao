ff15524ce4f785fded668bf869897cfe75e2fb43
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-16 15:15:53 -0300

    listas

--- lista aula 06/questao 01.py
+++ lista aula 06/questao 01.py
@@ -9,7 +9,6 @@ def lista_final(lista1,lista2):
     lista3 = []
     lista3.extend(lista1)
     lista3.extend(lista2)
-    lista3 = sorted(lista3)
     print(f"{lista3}")
 
 lista_final(lista1,lista2)
