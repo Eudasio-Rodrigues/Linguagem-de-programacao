71450decd674d5169d03f14fe1b58aa5f4d48a20
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-20 15:04:04 -0300

    dicionarios

---
+++ lista aula 07/questao02.py
@@ -0,0 +1,24 @@
+#Escreva uma função que faça a verificação de existência de uma chave 
+#dentro de um dicionário. Caso a chave exista a função deve retornar o 
+# valor da chave, caso ela não exista, a função deve adicioná-la e 
+# retornar o dicionário.
+
+dados_pessoais={'nome':"Eudasio",
+                "idade": 27,
+                "cidade":'Mombaça',
+                "estado_civil":'solteiro'
+                }
+
+chave=(input("Qual chave voce quer procurar\n"))
+
+def verifica_chave(dados_pessoais, chave):
+   
+    if chave in dados_pessoais:
+        print(dados_pessoais[chave])
+    else:
+        print("Chave não econtrada")
+        dados_pessoais[chave]=""
+        print(dados_pessoais)
+
+
+verifica_chave(dados_pessoais,chave)
