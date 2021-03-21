80afa2279aae3d6d0e7f4888756241a2cf909cdd
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-21 15:11:22 -0300

    questao 07

---
+++ lista aula 06/questao 07.py
@@ -0,0 +1,8 @@
+#Escreva uma função que receba uma lista de vários inteiros e retorne um único
+#inteiro composto por todos os itens da lista informada.
+a=[1,2,3,4]
+def f(l):
+    if not l: return 0
+    return l[-1] + f(l[:-1]) * 10
+
+print(f(a))
