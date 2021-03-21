d23f8adbb19edf17b75ee20cbe0c031a056cfddc
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-21 15:43:40 -0300

    questao 04

--- lista aula 07/questao 04.py
+++ lista aula 07/questao 04.py
@@ -1,10 +1,10 @@
 #Utilizando dic conprehension e list comprehension construa uma função que gere o seguinte dicionário: 
 #O dicionario deve ter como chave as letras de um nome inserido como argumento e 
 # como valor uma lista de pares dentro de um range, passado também como argumento
-lista_pares =[ x for x in range (0,100) if x%2==0]
+lista_pares =[ x for x in range (0,11) if x%2==0]
 nome='eudasio'
 def dicionario(nome,lista_pares):
-    dic = {nome:lista_pares}
+    dic = {x : lista_pares for x in nome}
     print(dic)
 
 dicionario(nome,lista_pares)
