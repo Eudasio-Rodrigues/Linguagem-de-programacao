4b81955d1dd70d2a5318bb5391fef89f41407485
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-21 15:01:58 -0300

    questao 03

--- lista aula 06/questao 03.py
+++ lista aula 06/questao 03.py
@@ -4,7 +4,10 @@
 lista = [1,2,2,5,9,0,0,0,10,3,3,3,3]
 
 def remove_repetidos(lista):
-    lista = set(lista)
-    print(lista)
+    lista2=[]
+    for i in lista:
+        if i not in lista2:
+            lista2.append(i)
+    print(lista2)
 
 remove_repetidos(lista)
