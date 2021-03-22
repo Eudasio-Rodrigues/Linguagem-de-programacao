b5bead295b545b80631b6645125699326798f1d9
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-22 15:27:05 -0300

    questao 01

--- lista aula 07/questao 01.py
+++ lista aula 07/questao 01.py
@@ -2,15 +2,15 @@
 #Em seguida utilize essas funções como argumentos de outra função que irá recebê-las e 
 #retornar um único dicionário
 
-def dic1(dicionario01):
+def dic1():
     dicionario01 = {x:x for x in range(0,11)}
     return dicionario01
 
-def dic02(dicionario02):
+def dic02():
     dicionario02 ={x:x for x in range(11,21)}
     return dicionario02
 
-def dic03(dicionario03):
+def dic03():
     dicionario03 ={x:x for x in range(21,31)}
     return dicionario03
     
@@ -19,4 +19,4 @@ def dic_unidas(dic1, dic02, dic03):
     dic02.update(dic03)
     print(dic1)
 
-dic_unidas(dic1('dicionario01'), dic02('dicionário02'), dic03('dicionário03'))
+dic_unidas(dic1(), dic02(), dic03())
