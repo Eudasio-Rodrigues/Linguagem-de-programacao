15a7f8b6989157bfabe566d1a8c05ab7b7b95f8b
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-22 15:25:56 -0300

    questao 04

--- lista aula 06/questao 04.py
+++ lista aula 06/questao 04.py
@@ -2,9 +2,14 @@
 #índices válidos dessa lista. A função deve retornar uma tupla composta 
 #pelos valores dos índices indicados.
 
-def converte(lista): 
-    return tuple(lista) 
-   
-lista = [1, 2, 3] 
-converte(lista)
-print(converte(lista)) 
+numeros = [x for x in range(1,11)]
+print(numeros)
+def converte(numeros, a ,b, c):
+    lista = []
+    lista.append(numeros[a])
+    lista.append(numeros[b])
+    lista.append(numeros[c])
+    nova_lista = tuple(lista)
+    print(nova_lista)
+
+converte(numeros, 2, 4, 6)
