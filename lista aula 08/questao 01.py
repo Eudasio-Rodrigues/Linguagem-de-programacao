5bb0b6fce60edc61dced88497cd2a13f604a3ab6
Eudasio-Rodrigues <rodrigues0117@gmail.com>
2021-03-23 15:13:45 -0300

    questao 01

---
+++ lista aula 08/questao 01.py
@@ -0,0 +1,71 @@
+#Crie um arquivo que contenha todos os números de de 2 a 1000: 
+#A partir desse arquivo gerado faça o que se pede:
+# Gere um arquivo apenas com múltiplos de 2
+# Inverta a ordem do arquivo de múltiplos de 2
+# Gere um arquivo com ímpares maiores que 10 e menores que 800
+# Gere um arquivo somente com números primos
+# Gere 3 arquivos, sendo (divisores de 2, 3 e 5)
+
+# Criação do arquivo
+with open("numeros.txt", "w") as numeros:
+    for i in range(2, 1001):
+
+        numeros.write(f"{i}\n")
+
+# Gerando os múltiplos
+with open("multiplos2.txt", "w") as multiplos2:
+    with open("numeros.txt") as numeros:
+        for i in numeros.readlines():
+
+            if int(i) % 2 == 0:
+                multiplos2.write(f"{i}")
+# Gerando os invertidos
+with open("multiplos_invertidos.txt", "w") as multiplos_invertidos:
+    with open("multiplos2.txt") as multiplos2:
+        l = []
+        for i in multiplos2.readlines():
+            l.append(int(i))
+
+        for i in l[::-1]:
+            multiplos_invertidos.write(f"{i}\n")
+# Impares
+with open("impares.txt", "w") as impares:
+    with open("numeros.txt") as numeros:
+        for i in numeros.readlines():
+            if int(i) % 2 != 0 and 10 < int(i) < 800:
+                impares.write(f"{i}")
+
+# Primos
+
+
+def primo(n):
+    cont = 0
+    i = 0
+    while i <= n or cont < 2:
+        i = i + 1
+        x = n % i
+        if x == 0:
+            cont = cont + 1
+
+    if cont <= 2:
+        primos.write(f"{n}\n")
+        print("\n")
+
+
+with open("primos.txt", "w") as primos:
+    with open("numeros.txt") as numeros:
+
+        for i in numeros:
+            primo(int(i))
+
+
+# multiplos
+with open("multiplos2.txt", "w") as m2, open("multiplos3.txt", "w") as m3, open("multiplos5.txt", "w") as m5:
+    with open("numeros.txt") as numeros:
+        for i in numeros.readlines():
+            if int(i) % 2 == 0:
+                m2.write(f"{int(i)}\n")
+            if int(i) % 3 == 0:
+                m3.write(f"{int(i)}\n")
+            if int(i) % 5 == 0:
+                m5.write(f"{int(i)}\n")
